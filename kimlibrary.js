10205d64a3bf02ded15c60985f4a14020dee0f71
Kim Kindya <kakindya@gmail.com>
2015-11-07 01:51:18 -0500

    Index page added all clickable objects and pdf download

--- kimlibrary.js
+++ kimlibrary.js
@@ -1,31 +1,17 @@
 $(document).ready(function() {
 
-// Location page: Map is hidden upon loading, then fades in over 2 sec
-
-		
+//Index page: map appears as a slide down, then the clickable objects fade in slightly later
+	
 
     $(".location").hide(0).delay(500).slideDown(2000);
 
     $(".mapoverlay1").hide(0).delay(1000).fadeIn(6000);
 
+    $(".mapoverlay2").hide(0).delay(2000).fadeIn(6000);
 
-// // Women and Men pages: enlargement of thumbnails on clicking
-//         $("body").on("contextmenu", ".enlarged-image", function() {
-//           return false;
-//         });
+	$(".mapoverlay3").hide(0).delay(3000).fadeIn(6000);
 
-//         $(".thumbnail").click(function() {
-//           $("body").css("overflow", "hidden");
-//           $(".lightbox .enlarged-image").attr("src", $(this).attr("src"));
-//           $(".lightbox").show();
-//         });
 
-// // Women and Men pages: Returns enlarged image to previous size
-//         $(".close").click(function() {
-//           $("body").css("overflow", "");
-//           $(".lightbox .enlarged-image").attr("src", "");
-//           $(".lightbox").hide();
-//         });
       });
 
 
